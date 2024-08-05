cf075dc5bd1b7fc633602eb50a38c21d153d9d3a
vgouttin <vgouttin7@gmail.com>
2024-08-04 21:27:20 -0600

    added trainline class

--- SubwayScreen/src/ca/ucalgary/ensf380/TrainLine.java
+++ SubwayScreen/src/ca/ucalgary/ensf380/TrainLine.java
@@ -1,5 +1,31 @@
 package ca.ucalgary.ensf380;
 
-public class TrainLine {
+import java.util.*;
 
+//TrainLine class
+public class TrainLine {
+	//Attributes
+	private String color;
+	private List<Station> lineStations;
+	private List<Station> commonLineStations;
+	
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public List<Station> getLineStations() {
+		return lineStations;
+	}
+	public void setLineStations(List<Station> lineStations) {
+		this.lineStations = lineStations;
+	}
+	public List<Station> getCommonLineStations() {
+		return commonLineStations;
+	}
+	public void setCommonLineStations(List<Station> commonLineStations) {
+		this.commonLineStations = commonLineStations;
+	}
+	
 }
